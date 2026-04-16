fd33ede008b6ac7ff7cd221abdca5fa7f0d08c82
MathN2 <matheus-novais-de-ramos@hotmail.com>
2026-04-16 05:39:00 -0300

    Correçao

--- main.py
+++ main.py
@@ -2,5 +2,4 @@ from cinema.UI.CLI.menu import menu_principal
 from cinema.services.filme_services import is_movie_active
 
 if __name__ == '__main__':
-    print(is_movie_active(13))
     menu_principal()
