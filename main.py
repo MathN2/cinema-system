9f481ccbce11f783eebe447a664975851632cf13
MathN2 <matheus-novais-de-ramos@hotmail.com>
2026-05-14 00:07:14 -0300

    correção da criação do db

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from cinema.UI.CLI.menu import menu_principal
-from cinema.UI.GUI.telas.tela_cliente import open_client
 from cinema.data.init_db import create_db
 
 if __name__ == '__main__':
     create_db()
-    # open_client()
+
+    from cinema.UI.CLI.menu import menu_principal
+    from cinema.UI.GUI.telas.tela_cliente import open_client
+
+    open_client()
     menu_principal()
